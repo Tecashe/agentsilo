ff3e1bc082b25e84ca855f5c2f0962e32f15aaf8
Macharia11 <stevieskaka3096@gmail.com>
2025-05-11 16:12:35 +0300

    Admins

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <Navbar />
           <AuthProvider>
-          {children}
+          <main>{children}</main>
           <Toaster />
           </AuthProvider>
         </ThemeProvider>
